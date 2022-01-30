a509bd9e9599031f64f0a9e39cda783f23c62d63
Sujit Jha <64594466+SujitJha2001@users.noreply.github.com>
2022-01-30 13:06:59 +0530

    Update CoronavirusTrackerApplication.java

--- src/main/java/io/javabrains/coronavirustracker/CoronavirusTrackerApplication.java
+++ src/main/java/io/javabrains/coronavirustracker/CoronavirusTrackerApplication.java
@@ -1,13 +1,17 @@
 package io.javabrains.coronavirustracker;
 
-import java.io.BufferedReader;
+
 import java.io.FileReader;
+import java.io.Reader;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.opencsv.CSVReader;
 
 import io.javabrains.coronavirustracker.models.LocationStats;
 import io.javabrains.coronavirustracker.repository.LocationStatsRepo;
@@ -22,124 +26,42 @@ public class CoronavirusTrackerApplication implements CommandLineRunner{
 	
 	@Autowired
 	private LocationStatsRepo obj;
-	
-	/*-----------------------*/
-	public boolean isBlank(String str) {
-        int strLen;
-        if (str == null || (strLen = str.length()) == 0) {
-            return true;
-        }
-        for (int i = 0; i < strLen; i++) {
-            if ((Character.isWhitespace(str.charAt(i)) == false)) {
-                return false;
-            }
-        }
-        return true;
-    }
-	/*-----------------------*/
 
-	String line="";
+	//String line="";
 	@Override
 	public void run(String... args) throws Exception {
-		BufferedReader br =new BufferedReader(new FileReader("src/main/resources/covid_vaccine_statewise.csv"));
-		
-		while((line=br.readLine())!=null) {
-			System.out.println(line);
-			String [] data = line.split(",");
+		Reader reader = new FileReader("src/main/resources/covid_vaccine_statewise.csv");
+        CSVReader csvReader = new CSVReader(reader);
+        String[] line;
+        List<String[]> l = csvReader.readAll();
+        l.remove(0);
+        for(String[] lines : l)
+        {
+        	LocationStats corona_data = new LocationStats();
+        	corona_data.setUpdated_On(lines[0]);
+			corona_data.setState(lines[1]);
+			corona_data.setTotal_Doses_Administered(lines[2]);
+			corona_data.setTotal_Sessions_Conducted(lines[3]);
+			corona_data.setTotal_Sites(lines[4]);
+			corona_data.setFirst_Dose_Administered(lines[5]);
+			corona_data.setSecond_Dose_Administered(lines[6]);
+			corona_data.setMale_Vaccinated(lines[7]);
+			corona_data.setFemale_Vaccinated(lines[8]);
+			corona_data.setTransgender_Vaccinated(lines[9]);
+			corona_data.setTotal_Covaxin_Administered(lines[10]);
+			corona_data.setTotal_CoviShield_Administered(lines[11]);
+			corona_data.setTotal_Sputnik_V_Administered(lines[12]);
+			corona_data.setAEFI(lines[13]);
+			corona_data.setAge_group1(lines[14]);
+			corona_data.setAge_group2(lines[15]);
+			corona_data.setAge_group3(lines[16]);
+			corona_data.setTotal_Individuals_Vaccinated(lines[17]);
 			
-			LocationStats corona_data = new LocationStats();
-			if (!isBlank(data[0]))
-			corona_data.setUpdated_On(data[0]);
-			else
-				corona_data.setUpdated_On("");
-			
-			if (!isBlank(data[1]))
-			corona_data.setState(data[1]);
-			else
-				corona_data.setState("");
-			
-			if (!isBlank(data[2]))
-			corona_data.setTotal_Doses_Administered(data[2]);
-			else
-				corona_data.setTotal_Doses_Administered("");
-			
-			if (!isBlank(data[3]))
-			corona_data.setTotal_Sessions_Conducted(data[3]);
-			else
-				corona_data.setTotal_Sessions_Conducted("");
-			
-			if (!isBlank(data[4]))
-			corona_data.setTotal_Sites(data[4]);
-			else
-				corona_data.setTotal_Sites("");
-			
-			if (!isBlank(data[5]))
-			corona_data.setFirst_Dose_Administered(data[5]);
-			else
-				corona_data.setFirst_Dose_Administered("");
-			
-			if (!isBlank(data[6]))
-			corona_data.setSecond_Dose_Administered(data[6]);
-			else
-				corona_data.setSecond_Dose_Administered("");
-			
-			if (!isBlank(data[7]))
-			corona_data.setMale_Vaccinated(data[7]);
-			else
-				corona_data.setMale_Vaccinated("");
-			
-			if (!isBlank(data[8]))
-			corona_data.setFemale_Vaccinated(data[8]);
-			else
-				corona_data.setFemale_Vaccinated("");
-			
-			if (!isBlank(data[9]))
-			corona_data.setTransgender_Vaccinated(data[9]);
-			else
-				corona_data.setTransgender_Vaccinated("");
-			
-			if (!isBlank(data[10]))
-			corona_data.setTotal_Covaxin_Administered(data[10]);
-			else
-				corona_data.setTotal_Covaxin_Administered("");
-			
-			if (!isBlank(data[11]))
-			corona_data.setTotal_CoviShield_Administered(data[11]);
-			else
-				corona_data.setTotal_CoviShield_Administered("");
-			
-			if (!isBlank(data[12]))
-			corona_data.setTotal_Sputnik_V_Administered(data[12]);
-			else
-				corona_data.setTotal_Sputnik_V_Administered("");
-			
-			if (!isBlank(data[13]))
-			corona_data.setAEFI(data[13]);
-			else
-				corona_data.setAEFI("");
-			
-			if (!isBlank(data[14]))
-			corona_data.setAge_group1(data[14]);
-			else
-				corona_data.setAge_group1("");
-			
-			if (!isBlank(data[15]))
-			corona_data.setAge_group2(data[15]);
-			else
-				corona_data.setAge_group2("");
-			
-			if (!isBlank(data[16]))
-			corona_data.setAge_group3(data[16]);
-			else
-				corona_data.setAge_group3("");
-			
-			if (!isBlank(data[17]))
-			corona_data.setTotal_Individuals_Vaccinated(data[17]);
-			else
-				corona_data.setTotal_Individuals_Vaccinated("");
 			
 			obj.save(corona_data);
-		}
-	}
+        }
+        reader.close();
+        csvReader.close();
+   	}
 
 }
